60ca8bacefa9309ff50a95dfd08ee6ad7158b4e6
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2018-04-25 11:49:10 +0300

    Moved the main logic into a separate header and object file.

---
+++ db_operations.c
@@ -0,0 +1,177 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#define MAX_DATA 512
+#define MAX_ROWS 100
+
+//declare struct for address record
+struct Address {
+	int id;
+	int set;
+	char name[MAX_DATA];
+	char email[MAX_DATA];
+};
+
+//declare struct that represents the database
+struct Database {
+	struct Address rows[MAX_ROWS];
+};
+
+//declare a struct that represents the DB connection to a file pointer and Database struct
+struct Connection {
+	FILE *file;
+	struct Database *db;
+};
+
+//function for error handling
+void die(const char *message, struct Connection *conn) {
+	//check if the errno variable is set (system variable)
+	if(errno) {
+		//if the system variable errno is set print the corresponding error message with perror
+		perror(message);
+	} else {
+		//otherwise print the string passed to the function
+		printf("ERROR: %s\n", message);
+	}
+
+	if (conn->db) {
+        free(conn->db);
+    }
+
+    if (conn->file) {
+        fclose(conn->file);
+    }
+
+	if (conn) {
+        free(conn);
+    }
+
+	exit(1);
+}
+
+//output the info about an address
+void Address_print(struct Address *addr) {
+	printf("%d %s %s\n", addr->id, addr->name, addr->email);
+}
+
+void Database_load(struct Connection *conn) {
+
+	//size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream)
+	/*
+		ptr − This is the pointer to a block of memory with a minimum size of size*nmemb bytes.
+		size − This is the size in bytes of each element to be read.
+		nmemb − This is the number of elements, each one with a size of size bytes.
+		stream − This is the pointer to a FILE object that specifies an input stream.
+	*/
+
+	if (!conn)
+        die("Failed to load database.", conn);
+
+	int rc = fread(conn->db, sizeof(struct Database), 1, conn->file);
+	if (rc != 1) die("Failed to load database.", conn);
+}
+
+//open database and return a connection
+struct Connection *Database_open(const char *filename, char mode) {
+	struct Connection *conn = malloc(sizeof(struct Connection));
+	if(!conn) die("Memory error!", conn);
+
+	conn->db = malloc(sizeof(struct Database));
+	if (!conn->db) {
+        die ("Memory error!", conn);
+	}
+
+	//open the file pointer in a different mode depending on the command
+	if (mode == 'c') {
+		//if we choose the create mode open in "write"
+		//if a file with the same name exists - truncate it
+		conn->file = fopen(filename, "w");
+	} else {
+		//if we choose another option open in r+ mode (for update, both for reading and writing)
+		conn->file = fopen(filename, "r+");
+
+		if(conn->file)
+			Database_load(conn);
+	}
+
+	if(!conn->file) die("Failed to open the file", conn);
+
+	return conn;
+}
+
+//free the Database and Connection structs from the heap
+void Database_close(struct Connection *conn) {
+	if(conn){
+		if(conn->file) fclose(conn->file);
+		if(conn->db) free(conn->db);
+		free(conn);
+	}
+}
+
+void Database_write(struct Connection *conn) {
+	rewind(conn->file);
+
+	int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
+	if(rc != 1 ) die("Failed to write database.", conn);
+
+	rc = fflush(conn->file);
+	if (rc == -1) die ("Cannot flush database.", conn);
+}
+
+void Database_create(struct Connection *conn) {
+	int i = 0;
+
+	for(i = 0; i < MAX_ROWS; i++) {
+		struct Address addr = {.id = i, .set = 0};
+		conn->db->rows[i] = addr;
+	}
+}
+
+void Database_set(struct Connection *conn, int id, const char *name, const char *email) {
+	struct Address *addr = &conn->db->rows[id];
+	if(addr->set) die("Already set, delete it first.", conn);
+
+	addr->set = 1;
+	char *res = strncpy(addr->name, name, MAX_DATA);
+	//Demonstrate the strncpy bug
+	if (!res) die("Name copy failed", conn);
+
+	/*
+        dest − This is the pointer to the destination array where the content is to be copied.
+        src − This is the string to be copied.
+        n − The number of characters to be copied from source.
+    */
+	res = strncpy(addr->email, email, MAX_DATA);
+	if (!res) die("Email copy failed", conn);
+}
+
+void Database_get(struct Connection *conn, int id) {
+	struct Address *addr = &conn->db->rows[id];
+
+	if(addr->set) {
+		Address_print(addr);
+	} else {
+		die("ID is not set", conn);
+	}
+}
+
+void Database_delete(struct Connection *conn, int id) {
+	struct Address addr = {.id = id, .set = 0};
+	conn->db->rows[id] = addr;
+}
+
+void Database_list(struct  Connection *conn) {
+	int i = 0;
+	struct Database *db = conn->db;
+
+	for (i = 0; i < MAX_ROWS; i++) {
+		struct Address *cur = &db->rows[i];
+
+		if(cur->set){
+			Address_print(cur);
+		}
+	}
+}
---
+++ db_operations.h
@@ -0,0 +1,23 @@
+//declare struct for address record
+struct Address;
+
+//declare struct for the database sizes
+struct Sizes;
+
+//declare struct that represents the database
+struct Database;
+
+//declare a struct that represents the DB connection to a file pointer and Database struct
+struct Connection;
+
+void die(const char *message, struct Connection *conn);
+void Address_print(struct Address *addr);
+void Database_load(struct Connection *conn);
+struct Connection *Database_open(const char *filename, char mode);
+void Database_close(struct Connection *conn);
+void Database_write(struct Connection *conn);
+void Database_create(struct Connection *conn);
+void Database_set(struct Connection *conn, int id, const char *name, const char *email);
+void Database_get(struct Connection *conn, int id);
+void Database_delete(struct Connection *conn, int id);
+void Database_list(struct  Connection *conn);
