2fa6124fa4691ef328d38bf1c57b837e7d3e522d
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2018-04-27 11:14:03 +0300

    Revert "moved the struct definitions to the proper place"
    This reverts commit 0cfdf37add8f39b5911382211ae7ad104ee82ede.

--- main.c
+++ main.c
@@ -21,7 +21,7 @@ int main(int argc, char** argv){
 	int id = 0;
 
 	if(argc > 3) id = atoi(argv[3]);
-	if(id >= conn->config->max_rows && action != 'c') die("There's not that many records", conn);
+	if(id >= MAX_ROWS && action != 'c') die("There's not that many records", conn);
 
 	int max_rows;
 	int max_data;
